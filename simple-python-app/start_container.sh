cdcc8d0026916d5f3ca3fcce583415de02c4657d
ArunAsam <arunanjaiahasam@gmail.com>
2024-08-23 15:23:17 +0530

    adding files for AWScodedeploy

--- simple-python-app/start_container.sh
+++ simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull arunasam/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 arunasam/simple-python-flask-app
